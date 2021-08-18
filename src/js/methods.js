0e07278fb0b96b6787e3d5d58b7c8a340c18a546
Cassiano - Manjaro <rodrigues.cassiano2007@gmail.com>
2021-08-18 18:29:56 -0300

    fix: fixed typos

--- src/js/methods.js
+++ src/js/methods.js
@@ -48,7 +48,8 @@ function rightRectangleRuleMethod({ func, a, b, epsilon }) {
 
 function trapezoidolRuleMethod({ func, a, b, epsilon }) {
   let n = 10;
-  let p = (mathFunction(func, [a])[0] + mathFunction(func, [b])[0]) / 2, q;
+  let p = (mathFunction(func, [a])[0] + mathFunction(func, [b])[0]) / 2,
+    q;
   let h = (b - a) / n;
   for (let i = 0; i < n; i++) {
     x = a + i * h;
@@ -69,30 +70,48 @@ function trapezoidolRuleMethod({ func, a, b, epsilon }) {
   return p;
 }
 
-function simpson13RuleMethod(func, a, b, erro) {
-  let n = 10, p = 0, q;
+function simpson13RuleMethod({ func, a, b, epsilon }) {
+  let n = 10,
+    p = 0,
+    q;
   let h = (b - a) / n;
 
   for (let i = 0; i < n; i += 2) {
-    p += (h / 3) * (mathFunction(func, [a + i * h])[0] + 4 * mathFunction(func, [a + (i + 1) * h])[0] + mathFunction(func, [a + (i + 2) * h])[0]);
+    p +=
+      (h / 3) *
+      (mathFunction(func, [a + i * h])[0] +
+        4 * mathFunction(func, [a + (i + 1) * h])[0] +
+        mathFunction(func, [a + (i + 2) * h])[0]);
   }
   do {
     q = p;
     n *= 2;
     p = 0;
     for (let i = 0; i < n; i += 2) {
-      p += (h / 3) * (mathFunction(func, [a + i * h])[0] + 4 * mathFunction(func, [a + (i + 1) * h])[0] + mathFunction(func, [a + (i + 2) * h])[0]);
+      p +=
+        (h / 3) *
+        (mathFunction(func, [a + i * h])[0] +
+          4 * mathFunction(func, [a + (i + 1) * h])[0] +
+          mathFunction(func, [a + (i + 2) * h])[0]);
     }
-  } while (Math.abs(p - q) > erro);
+  } while (Math.abs(p - q) > epsilon);
   return p;
 }
 
 function simpson38RuleMethod({ func, a, b, epsilon }) {
-  let n = 15, p = 0, q;
+  let n = 15,
+    p = 0,
+    q;
   let h = (b - a) / n;
 
   for (let i = 0; i < n; i += 3) {
-    p += (3 / 8) * h * (mathFunction(func, [a + i * h])[0] + 3 * mathFunction(func, [a + (i + 1) * h])[0] + 3 * mathFunction(func, [a + (i + 2) * h])[0] + mathFunction(func, [a + (i + 3) * h])[0]);
+    p +=
+      (3 / 8) *
+      h *
+      (mathFunction(func, [a + i * h])[0] +
+        3 * mathFunction(func, [a + (i + 1) * h])[0] +
+        3 * mathFunction(func, [a + (i + 2) * h])[0] +
+        mathFunction(func, [a + (i + 3) * h])[0]);
   }
   do {
     q = p;
@@ -100,16 +119,25 @@ function simpson38RuleMethod({ func, a, b, epsilon }) {
     let h = (b - a) / n;
     p = 0;
     for (let i = 0; i < n; i += 3) {
-      p += (3 / 8) * h * (mathFunction(func, [a + i * h])[0] + 3 * mathFunction(func, [a + (i + 1) * h])[0] + 3 * mathFunction(func, [a + (i + 2) * h])[0] + mathFunction(func, [a + (i + 3) * h])[0]);
+      p +=
+        (3 / 8) *
+        h *
+        (mathFunction(func, [a + i * h])[0] +
+          3 * mathFunction(func, [a + (i + 1) * h])[0] +
+          3 * mathFunction(func, [a + (i + 2) * h])[0] +
+          mathFunction(func, [a + (i + 3) * h])[0]);
     }
   } while (Math.abs(p - q) > epsilon);
   return p;
 }
 
-function GaussQuadratureMethod(func, a, b) {
+function GaussQuadratureMethod({ func, a, b }) {
   let p = 0;
   for (let i = 0; i < 20; i++) {
-    p += w[i] * (((b - a) / 2) * mathFunction(func, [a * ((1 - X[i]) / 2) + b * ((1 + X[i]) / 2)])[0]);
+    p +=
+      w[i] *
+      (((b - a) / 2) *
+        mathFunction(func, [a * ((1 - X[i]) / 2) + b * ((1 + X[i]) / 2)])[0]);
   }
   return p;
 }
